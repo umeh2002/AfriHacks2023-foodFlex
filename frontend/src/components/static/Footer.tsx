1da3840dccff27dc8b4c3869bb032b33c7167288
unknown <emmanuelrich206@gmail.com>
2023-12-11 01:44:39 +0100

    landingpage

---
+++ frontend/src/components/static/Footer.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+  return (
+    <div className="w-full py-5 flex flex-col items-center text-white bg-[#424242] justify-center rounded-t-[50px]">
+        <div className="flex flex-col items-center">
+        <p className="text-[30px] font-bold">FoodFlex</p>
+            <p className="text-[13px]">lagos,Nigeria</p>
+            <p className="w-[50%] text-center mt-3 text-[13px]">your gateway to stress-free grocery shopping with instant loans. Enjoy the freedom to purchase quality groceries without the financial constraints.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,23 @@
-const LandingPage = () => {
-  return <>LandingPage</>;
-};
+import AboutPage from "../components/AboutPage"
+import Footer from "../components/static/Footer"
+import Header from "../components/static/Header"
+import Hero from "../components/static/Hero"
+import ProductScreen from "../components/static/ProductScreen"
 
-export default LandingPage;
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Hero/>
+      <br />
+      <br />
+      <AboutPage/>
+      <br />
+      <ProductScreen/>
+      <br />
+      <Footer/>
+    </div>
+  )
+}
+
+export default LandingPage
